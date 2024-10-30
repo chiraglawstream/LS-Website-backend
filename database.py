1d9b3bfc23a2e95c3806c22fc0f7c4cd55a5422d
chirag <chirag.yadav@lawstream.in>
2024-10-30 23:10:10 +0530

    updates

--- database.py
+++ database.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
+mongo_uri = os.getenv("MONGO_URI")
 
 def get_database():
     try:
-        mongo_uri = os.getenv("MONGO_URI")
         client = MongoClient(mongo_uri, tls=True, tlsAllowInvalidCertificates=True)
         db = client["contact_us_db"]
-        return db["contacts"]  # Return the specific collection
+        return db["contacts"] 
     except Exception as e:
         print(f"Database connection error: {e}")
         raise

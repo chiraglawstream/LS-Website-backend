3a10f784a62713b5c96ca7bb4cc7f8324b4fb1fc
chirag <chirag.yadav@lawstream.in>
2024-10-30 22:38:18 +0530

    updates

--- models/contact_us.py
+++ models/contact_us.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field, root_validator
+from typing import List, Optional
+from datetime import datetime
 
 # Pydantic model for validating the form data
 class ContactForm(BaseModel):
